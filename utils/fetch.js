374f64308d60ac8722ac2e058b14929fca8cf323
Michi Berger <mberger83@yahoo.de>
2022-02-04 16:26:12 +0100

    Write javascript for cardss in fetch.js

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,11 +1,30 @@
-export default function fetchAll() {
-  const apiUrl = 'https://swapi.dev/api/people';
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => createPeopleList(data.results))
-    .catch(error => console.log(error));
+import fetchCharacters from '../services/fetchCharacters.js';
+
+export default async function fetchAll() {
+  const people = await fetchCharacters();
+  createPeopleList(people);
 
   function createPeopleList(people) {
-    console.log(people);
+    const sliceApi = people.slice(0, 10);
+    console.log(sliceApi);
+
+    const singleCard = document.querySelector('[data-js="Card"]');
+    console.log(singleCard);
+
+    //bild greifen und zwischen speichern in einer Konstanten
+    const imageOfCharacter = sliceApi[0].image;
+    console.log(imageOfCharacter);
+
+    //namen greifen und in einer Konstanten
+    const nameOfCharacter = sliceApi[0].name;
+    console.log(nameOfCharacter);
+
+    //Karte erstellen
+
+    //Bild und Name einfügen
+
+    //Karte interaktiv machen
+
+    //die Karte in unseren Browser anzeigen
   }
 }
